918d74cecb1357e2a9736e2871dde5b2b3fb2d2f
LonelyWhale <joshua.buckledee@live.co.uk>
2019-01-30 20:17:39 +0000

    Added text to Conservative party page.

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f070022;
         public static final int end = 0x7f070039;
         public static final int gone = 0x7f070040;
-        public static final int invisible = 0x7f07005b;
-        public static final int left = 0x7f07005e;
-        public static final int packed = 0x7f07006e;
-        public static final int parent = 0x7f07006f;
-        public static final int percent = 0x7f070071;
-        public static final int right = 0x7f070075;
-        public static final int spread = 0x7f07008f;
-        public static final int spread_inside = 0x7f070090;
-        public static final int start = 0x7f070095;
-        public static final int top = 0x7f0700ac;
-        public static final int wrap = 0x7f0700b3;
+        public static final int invisible = 0x7f07005e;
+        public static final int left = 0x7f070061;
+        public static final int packed = 0x7f070071;
+        public static final int parent = 0x7f070072;
+        public static final int percent = 0x7f070074;
+        public static final int right = 0x7f070078;
+        public static final int spread = 0x7f070091;
+        public static final int spread_inside = 0x7f070092;
+        public static final int start = 0x7f070097;
+        public static final int top = 0x7f0700b4;
+        public static final int wrap = 0x7f0700bb;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coreutils/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coreutils/R.java
@@ -28,10 +28,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f04003f;
-        public static final int notification_icon_bg_color = 0x7f040040;
-        public static final int ripple_material_light = 0x7f04004a;
-        public static final int secondary_text_default_material_light = 0x7f04004c;
+        public static final int notification_action_color_filter = 0x7f040040;
+        public static final int notification_icon_bg_color = 0x7f040041;
+        public static final int ripple_material_light = 0x7f04004b;
+        public static final int secondary_text_default_material_light = 0x7f04004d;
     }
     public static final class dimen {
         private dimen() {}
@@ -87,25 +87,25 @@ public final class R {
         public static final int blocking = 0x7f070021;
         public static final int chronometer = 0x7f07002a;
         public static final int forever = 0x7f07003f;
-        public static final int icon = 0x7f070052;
-        public static final int icon_group = 0x7f070053;
-        public static final int info = 0x7f07005a;
-        public static final int italic = 0x7f07005c;
-        public static final int line1 = 0x7f07005f;
-        public static final int line3 = 0x7f070060;
-        public static final int normal = 0x7f07006a;
-        public static final int notification_background = 0x7f07006b;
-        public static final int notification_main_column = 0x7f07006c;
-        public static final int notification_main_column_container = 0x7f07006d;
-        public static final int right_icon = 0x7f070076;
-        public static final int right_side = 0x7f070077;
-        public static final int tag_transition_group = 0x7f070099;
-        public static final int tag_unhandled_key_event_manager = 0x7f07009a;
-        public static final int tag_unhandled_key_listeners = 0x7f07009b;
-        public static final int text = 0x7f07009c;
-        public static final int text2 = 0x7f07009d;
-        public static final int time = 0x7f0700a8;
-        public static final int title = 0x7f0700a9;
+        public static final int icon = 0x7f070054;
+        public static final int icon_group = 0x7f070055;
+        public static final int info = 0x7f07005d;
+        public static final int italic = 0x7f07005f;
+        public static final int line1 = 0x7f070062;
+        public static final int line3 = 0x7f070063;
+        public static final int normal = 0x7f07006d;
+        public static final int notification_background = 0x7f07006e;
+        public static final int notification_main_column = 0x7f07006f;
+        public static final int notification_main_column_container = 0x7f070070;
+        public static final int right_icon = 0x7f070079;
+        public static final int right_side = 0x7f07007a;
+        public static final int tag_transition_group = 0x7f07009b;
+        public static final int tag_unhandled_key_event_manager = 0x7f07009c;
+        public static final int tag_unhandled_key_listeners = 0x7f07009d;
+        public static final int text = 0x7f07009e;
+        public static final int text2 = 0x7f07009f;
+        public static final int time = 0x7f0700b0;
+        public static final int title = 0x7f0700b1;
     }
     public static final class integer {
         private integer() {}
@@ -125,7 +125,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0b002e;
+        public static final int status_bar_notification_info_overflow = 0x7f0b002f;
     }
     public static final class style {
         private style() {}
