4cfb1e66c48d5826f7d5a6740aa22cd50b999a46
LonelyWhale <joshua.buckledee@live.co.uk>
2019-03-16 15:39:25 +0000

    Added all UK party links.

--- app/src/main/java/com/terawhale/appsforgood/whatpolitics/England.java
+++ app/src/main/java/com/terawhale/appsforgood/whatpolitics/England.java
@@ -12,15 +12,4 @@ public class England extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_england);
     }
-    public void conservative(View view) {
-        Intent conservativePage = new Intent(this,conservativePage.class);
-        startActivity(conservativePage);
-        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
-
-    }
-    public void labour(View view) {
-        Intent labourPage = new Intent(this, labourPage.class);
-        startActivity((labourPage));
-        overridePendingTransition(R.anim.slide_in_bottom,R.anim.slide_out_top);
-    }
 }
--- app/src/main/java/com/terawhale/appsforgood/whatpolitics/UKParties.java
+++ app/src/main/java/com/terawhale/appsforgood/whatpolitics/UKParties.java
@@ -1,7 +1,9 @@
 package com.terawhale.appsforgood.whatpolitics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UKParties extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class UKParties extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ukparties);
+    }
+    public void conservative(View view) {
+        Intent conservativePage = new Intent(this,conservativePage.class);
+        startActivity(conservativePage);
+        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+
+    }
+    public void labour(View view) {
+        Intent labourPage = new Intent(this, labourPage.class);
+        startActivity((labourPage));
+        overridePendingTransition(R.anim.slide_in_bottom,R.anim.slide_out_top);
     }
 }
